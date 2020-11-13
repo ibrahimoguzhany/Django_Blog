5448abbd200c1078fbd4d67e960f6a3b2572f7e5
İbrahim Oğuzhan YILMAZ <05180000474@ogrenci.ege.edu.tr>
2020-11-13 12:55:50 +0300

    second commit

--- article/models.py
+++ article/models.py
@@ -11,4 +11,21 @@ class Article(models.Model):
     article_image = models.FileField(blank=True, null=True, verbose_name="Makaleye Fotograf Ekleyin")
     def __str__(self): 
         return self.title
+    class Meta:
+        ordering = ['-created_date']
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name="Makale", related_name="comments")
+    comment_author = models.CharField(max_length = 50, verbose_name = "Isim")
+    comment_content = models.CharField(max_length = 200, verbose_name = "Yorum")
+    comment_date = models.DateTimeField(auto_now_add = True)
+    def __str__(self):
+        return self.comment_content
+    class Meta:
+        ordering = ['-comment_date']
+
+
+
+
+
 
--- article/views.py
+++ article/views.py
@@ -1,29 +1,49 @@
-from django.shortcuts import render,HttpResponse,redirect,get_object_or_404
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404,reverse
 from .forms import ArticleForm
-from .models import Article
+from .models import Article,Comment
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+def articles(request):
+    keyword = request.GET.get("keyword")
+
+    if keyword:
+        articles = Article.objects.filter(title__contains=keyword)
+        return render(request, "articles.html", {"articles": articles})
+
+    articles = Article.objects.all()
+
+    return render(request, "articles.html", {"articles": articles})
+
 
 def index(request):
 
     context = {
-        "number1":10,
-        "number2":20
+        "number1": 10,
+        "number2": 20
     }
 
-    return render(request, "index.html", context) 
+    return render(request, "index.html", context)
+
 
 def about(request):
     return render(request, "about.html")
 
-def dashboard(request):
-    articles = Article.objects.filter(author = request.user)
-    context = {
-        "articles":articles
-    }
-    return render(request,"dashboard.html",context)
 
+@login_required(login_url="user:login")
+def dashboard(request):
+
+    articles = Article.objects.filter(author=request.user)
+    context = {
+        "articles": articles
+    }
+    return render(request, "dashboard.html", context)
+
+
+@login_required(login_url="user:login")
 def addarticle(request):
     form = ArticleForm(request.POST or None, request.FILES or None)
 
@@ -34,33 +54,60 @@ def addarticle(request):
         article.save()
 
         messages.success(request, "Makale basariyla olusturuldu.")
-        return redirect("index")
+        return redirect("article:dashboard")
 
-    return render(request,"addarticle.html",{"form":form})
+    return render(request, "addarticle.html", {"form": form})
 
-def detail(request,id):
+
+def detail(request, id):
     # article = Article.objects.filter(id = id).first()
-    article = get_object_or_404(Article, id = id)
-    return render(request, "detail.html", {"article":article})
+    article = get_object_or_404(Article, id=id)
+
+    comments = article.comments.all()
 
 
+    return render(request, "detail.html", {"article": article,"comments":comments})
+
+
+@login_required(login_url="user:login")
 def updateArticle(request, id):
 
-    article = get_object_or_404(Article, id = id)
-    form = ArticleForm(request.POST or None, request.FILES or None,instance = article)
+    article = get_object_or_404(Article, id=id)
+    form = ArticleForm(request.POST or None,
+                       request.FILES or None, instance=article)
 
     if form.is_valid():
-        
+
         article = form.save(commit=False)
 
         article.author = request.user
         article.save()
 
         messages.success(request, "Makale basariyla guncellendi.")
-        return redirect("index")
+        return redirect("article:dashboard")
 
-    return render(request,"update.html",{"form": form})
-
-
+    return render(request, "update.html", {"form": form})
 
 
+@login_required(login_url="user:login")
+def deleteArticle(request, id):
+
+    article = get_object_or_404(Article, id=id)
+    article.delete()
+    messages.success(request, "Makale basariyla silinmistir")
+    return redirect("article:dashboard")
+
+def addComment(request,id):
+    article = get_object_or_404(Article,id = id)
+
+    if request.method == "POST":
+        comment_author = request.POST.get("comment_author")
+        comment_content = request.POST.get("comment_content")
+
+        newComment = Comment(comment_author = comment_author, comment_content = comment_content)
+
+        newComment.article = article
+
+        newComment.save()
+
+    return redirect(reverse("article:detail", kwargs={"id":id}))
